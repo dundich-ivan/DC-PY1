9841d02a3a0c84405dbd01aec2e073dba6aaab00
dundich-ivan <115348644+dundich-ivan@users.noreply.github.com>
2022-10-14 19:49:44 +0300

    Лабораторная работа 3

---
+++ Лабораторная работа 3/task1.py
@@ -0,0 +1,8 @@
+src = not False and True or False and not True
+
+
+
+result = False and True or False and True  # TODO подставить результат выражения
+result = False and True
+result = True
+print(src == result)
